52f7b23071ab15d7672ab1fc836f1d29bd80eed1
André Schmidt <mstra1040@gmail.com>
2022-05-20 00:46:41 -0300

    Periodo 2 adicionado

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import pandas as pd
 
 dados = pd.read_csv('dados.csv', sep=';', header=[0])
 
+print('**************************************')
+
 for i in range(len(dados)):
     print(dados['Simbolo'][i] + ' - ' + dados['Nome'][i])
     print('Número atômico: ' + str(dados['Numero'][i]))
     print('Série química: ' + dados['Serie quimica'][i])
     print('Grupo: ' + str(dados['Grupo'][i]))
-    print('')
+    print('Período: ' + str(dados['Periodo'][i]))
+    print('--------------------------------------')
+
+print('**************************************')
